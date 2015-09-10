13951d8eeb33a03dbf31291cdd363e0b25f093ab
Danilo Lima <danlima69@gmail.com>
2015-09-10 01:23:56 -0300

    Added Flask microframework

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Insult App',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Insults You',
+      author_email='dflima23@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask==0.10.1'],
      )
