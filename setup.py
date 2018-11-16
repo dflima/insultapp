26d86d84948186efa350a69b7725b1abedc3333a
Danilo <danlima69@gmail.com>
2018-11-16 14:15:19 -0200

    Update setup.py
    Updated Flask version regarding a security issue.

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='Insult App',
       author='Insults You',
       author_email='dflima23@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask==0.10.1'],
+      install_requires=['flask>=0.12.3'],
      )
