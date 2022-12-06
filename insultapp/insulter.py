a80035962e825301cec9b884dac4687fc8b4005c
Danilo Lima <danilo.lima@huspy.io>
2022-12-06 11:26:46 -0300

    Changed insults to f-strings

--- insultapp/insulter.py
+++ insultapp/insulter.py
@@ -1,14 +1,17 @@
 from random import choice
 
-def insult():
-    return "Thou " + generate_insult() + "!"
 
-def named_insult(name):
-    return name + ", thou " + generate_insult() + "!"
+def insult() -> str:
+    return f"Thou {generate_insult()}!"
 
-def generate_insult():
-    first_adjs = ["artless", "bawdy", "beslubbering", "bootless", "churlish"]
-    second_adjs = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained"]
+
+def named_insult(name: str) -> str:
+    return f"{name}, thou {generate_insult()}!"
+
+
+def generate_insult() -> str:
+    first_adjective_list = ["artless", "bawdy", "beslubbering", "bootless", "churlish"]
+    second_adjective_list = ["base-court", "bat-fowling", "beef-witted", "beetle-headed", "boil-brained"]
     nouns = ["apple-john", "baggage", "barnacle", "bladder", "boar-pig"]
 
-    return choice(first_adjs) + " " + choice(second_adjs) + " " + choice(nouns)
+    return f"{choice(first_adjective_list)} {choice(second_adjective_list)} {choice(nouns)}"
